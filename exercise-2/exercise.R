63934ebdc098f02bac8ff348257354706a5111f6
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-04-05 17:47:10 -0700

    labExcercise

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,46 +2,51 @@
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-CompareLength <- function(v1, v2) {
-  diff <- abs(length(v1) - length(v2))
-  diff.string <- paste('The difference in lengths is', diff)
-  return(diff.string)
-}  
+
+CompareLength <- function(a,b) {
+  x = length(a)
+  y = length(b)
+  n = abs(x - y)
+  ans <- paste("the difference in lengths is", n)
+  return (ans)
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-CompareLength(c(1,2,3), 1:100)
+a <- c(1,2,3)
+b <- c(1,2)
+difference <- CompareLength(a,b)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
 DescribeDifference <- function(a,b) {
-  diff <- length(a) - length(b)
-  if(diff > 0) {
-    sentence <- paste('Your first vector is longer by', diff, 'elements')
-  } else {
-    sentence <- paste('Your second vector is longer by', -diff, 'elements')
+  x = length(a)
+  y = length(b)
+  n = x-y
+  if (n>0) {
+    return (paste("Your first vector is longer by ", n, "elements"))
+  } else if (n<0) {
+    return (paste("Your second vector is longer by ", abs(n), "elements"))
   }
-  return(sentence)
+  return (paste("Your vectors have same length"))
 }
 
 # Pass two vectors to your `DescribeDifference` function
-DescribeDifference(1:15, 9:17)
+DescribeDifference(b,a)
 
 ### Bonus ###
 
 # Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
-# This helps: http://stackoverflow.com/questions/5754367/using-substitute-to-get-argument-name-with
-DescribeDifference <- function(a,b) {
-  diff <- length(a) - length(b)
-  if(diff > 0) {
-    vector.name <- deparse(substitute(a))
-    sentence <- paste('Vector', vector.name, 'is longer by', diff, 'elements')
-  } else {
-    vector.name <- deparse(substitute(b))
-    sentence <- paste('Vector', vector.name, 'is longer by', -diff, 'elements')
+DescribeDifference <- function (a,b) {
+  x = length(a)
+  y = length(b)
+  n = x-y
+  if (n>0) {
+    ans <- deparse(substitute(a))
+    return (paste(ans, "is longer by ", n, "elements"))
+  } else if (n<0){
+    ans <- deparse(substitute(b))
+    return (paste(ans , "is longer by ", abs(n), "elements"))
   }
-  return(sentence)
+  return (paste("two vectors have same length"))
 }
-x <- 1:15
-y <- 9:170
-DescribeDifference(x, y)
